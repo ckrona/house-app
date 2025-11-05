c894dcb2a26227271dde798ee3c1db3cf95609fb
ckrona <romkot369@gmail.com>
2025-11-05 21:15:37 +0300

    Update sendToTelegram.js

--- netlify/functions/sendToTelegram.js
+++ netlify/functions/sendToTelegram.js
@@ -1,6 +1,6 @@
 export async function handler(event, context) {
-  const TELEGRAM_TOKEN = "ТВОЙ_ТОКЕН";
-  const CHAT_ID = "ТВОЙ_CHAT_ID";
+  const TELEGRAM_TOKEN = "8512288959:AAGzU-9zvE6uprOXz7tBsz9EPvCudpC6BT8";
+  const CHAT_ID = "-4780600720";
 
   const data = JSON.parse(event.body || "{}");
 
